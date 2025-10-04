ec4654b64896757a6dc4a9ea2531834077c5177e
Anuja-Fernando <anujafernando35@gmail.com>
2025-10-04 21:05:30 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-4xl text-blue-600 bg-yellow-100 p-6 rounded">
+      Tailwind is working!
+    </div>
+  )
+}
+
+export default App
