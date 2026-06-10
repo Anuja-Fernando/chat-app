1523d492d8e99d9df370d3211e64cc646d7f6eda
Anuja-Fernando <anujafernando35@gmail.com>
2026-06-10 17:26:02 +0530

    Fix route handling for production environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,12 +31,11 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get("/:path(*)", (req, res) => {
+    app.get("/{*path}", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
+    });
 }
 
 // start server AFTER DB connects
